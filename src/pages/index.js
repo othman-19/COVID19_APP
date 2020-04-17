54bd4f1c60cc585a68a4eb4c29c333172f49a50c
othmane <othman-19@hotmail.fr>
2020-04-17 15:59:00 +0100

    trasform the fetched data into geaographic data

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,6 +55,27 @@ const IndexPage = () => {
       return;
     }
     const { data = [] } = response;
+    const hasData = Array.isArray(data) && data.length > 0;
+    if (!hasData) return;
+
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+    console.log(geoJson);
     // if ( !leafletElement ) return;
 
     // const popup = L.popup({
